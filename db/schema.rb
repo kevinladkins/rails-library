c32ced182c1fdf642a41325f5bec0281817f52b5
Kevin Adkins <kevinladkins@gmail.com>
2017-04-14 17:45:50 +0000

    Add publication_year and views to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414173711) do
+ActiveRecord::Schema.define(version: 20170414174419) do
 
   create_table "authors", force: :cascade do |t|
     t.datetime "born"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20170414173711) do
     t.text     "synopsis"
     t.integer  "status"
     t.integer  "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "publication_year"
   end
 
   create_table "users", force: :cascade do |t|
