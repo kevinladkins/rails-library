171eafec254bc4bde80bbe816a40f7276defd5be
Kevin Adkins <kevinladkins@gmail.com>
2017-04-14 17:22:14 +0000

    Clean up routes:

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :users, path_names: {new: 'signup'}
-  resources :sessions, only: [:new, :create, :destroy], path_names: {new: 'login', destroy: 'logout'}
+  resources :users, only: [:index, :create, :edit, :update, :delete]
+  resources :sessions, only: :create
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
   get 'auth/:provider/callback' => 'sessions#create'
+
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
