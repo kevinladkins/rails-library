54caab82f8b96ed25331b7e888afadf2838dd614
Kevin Adkins <kevinladkins@gmail.com>
2017-04-15 20:46:47 +0000

    Build loan#create

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,6 +1,20 @@
 class LoansController < ApplicationController
 
   def create
-    binding.pry 
+    loan = Loan.new
+    loan.book = Book.find(params[:book_id])
+    loan.patron = current_user
+    loan.checkout_date = DateTime.now
+    loan.checkout_date = DateTime.now + 14
+    loan.save
+    redirect_back(fallback_location: root_path)
   end
+
+
+  private
+
+  def loan_params
+    params.require(:book_id)
+  end
+
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,5 +2,21 @@ class Loan < ApplicationRecord
   belongs_to :patron, class_name: "User"
   belongs_to :book
 
-  
+  validates :patron_id, :book_id, presence: :true
+  validate :book_is_available, unless: :persisted?
+  before_create :take_book
+
+
+  def book_is_available
+    if !self.book.available?
+      errors.add(:book_id, "This book is not currently available")
+    end
+  end
+
+  def take_book
+
+    book.update(quantity: book.quantity - 1)
+  end
+
+
 end
