8e0e4013d39ebdb633813c429be6d06faf9b613a
Kevin Adkins <kevinladkins@gmail.com>
2017-04-18 17:36:01 -0400

    Build categories_controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,15 @@
+class CategoriesController < ApplicationController
+
+
+  def index
+    @categories = Categories.all
+  end
+  
+  
+  def show
+    @category = Category.find(params[:id]
+  end
+
+
+
+end
