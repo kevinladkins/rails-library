6c2ff2daa4dd1501801dd1e806ca5b0038eefc1d
Kevin Adkins <kevinladkins@gmail.com>
2017-04-18 17:32:49 -0400

    Add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,9 @@ class Category < ApplicationRecord
   has_many :books, through: :book_categories
 
   enum classification: [:fiction, :non_fiction]
+  
+  validates :name, :classification, presence: true
+  validates :name, uniqueness: true
 
   scope :fiction, -> {where(status: "fiction")}
   scope :non_fiction, -> {where(status: "non_fiction")}
