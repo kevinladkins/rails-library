5e49fc4a9118f8ad8196db9d4e5fa142676e7d63
Kevin Adkins <kevinladkins@gmail.com>
2017-04-21 12:43:52 -0400

    Clean up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if @user != current_user && !current_user.librarian?
-      flash.alert = "You cannot view another user's account"
+      flash.alert = "You cannot view another user's account."
       redirect_to root_path
     end
   end
