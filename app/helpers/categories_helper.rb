4da4a3e22b45a7af4e601656c79cc86408102f8e
Kevin Adkins <kevinladkins@gmail.com>
2017-04-20 20:59:08 -0400

    Build helpers for fiction/non-fiction categories

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -4,6 +4,14 @@ module CategoriesHelper
    categories = Category.find_each
    selection = alphabetize_by(categories, :name)
   end
+  
+  def fiction_categories
+    classification_list(@categories, "fiction", :name)
+  end
+  
+  def non_fiction_categories
+    classification_list(@categories, "non_fiction", :name)
+  end
 
 
 end
