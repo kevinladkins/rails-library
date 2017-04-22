63a115de19fc3b9ba45764bdce34d9ca62491497
Kevin Adkins <kevinladkins@gmail.com>
2017-04-21 20:10:14 -0400

    Refactor

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -2,7 +2,8 @@ module AuthorsHelper
 
   def authors_for_selection
 		authors = Author.find_each
-		selection = alphabetize_by(authors, :name)
+		selection = alphabetize_by(authors, :last_name)
   end
 
 end
+
