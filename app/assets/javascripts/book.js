a7fd808b068e34df7227716e828e43a0deb8ae92
github username <github email address>
2017-06-01 17:32:48 -0400

    Add book.js to javascripts, test functionality

---
+++ app/assets/javascripts/book.js
@@ -0,0 +1,12 @@
+$(function() {
+  $("#books-nav").click(function(e) {
+    e.preventDefault();
+    $("#view-content").html("")
+  })
+})
+
+$(function() {
+    $("h1").click(function() {
+    $("h1").html("Seriously????");
+  });
+});
