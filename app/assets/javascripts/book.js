aff10b227a30ff5d32b235e4c23c3ada823e4540
github username <github email address>
2017-06-01 17:52:31 -0400

    Build js Book prototype

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -10,3 +10,16 @@ $(function() {
     $("h1").html("Seriously????");
   });
 });
+
+function Book(book) {
+  this.id = book.id
+  this.title = book.title
+  this.synopsis = book.synopsis
+  this.available_copies = book.available_copies
+  this.publication_year = book.publication_year
+  this.classifcation = book.classification
+  this.copies = book.copies
+  this.author = book.author.name
+  this.authorId = book.author.id
+  this.categories = book.categories
+}
