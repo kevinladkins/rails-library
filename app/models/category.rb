f900546a28acbdd37a0e655236853b2fa086f1a0
github username <github email address>
2017-06-02 19:36:20 -0400

    Build #next method for Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,24 @@
 class Category < ApplicationRecord
-  
+
   enum classification: {fiction: 0, non_fiction: 1}
-  
+
   has_many :book_categories
   has_many :books, through: :book_categories
-  
+
   validates :name, :classification, presence: true
   validates :name, uniqueness: true
+
+  def self.next(id)
+    a = id +1
+    b = self.last.id
+    for i in a..b
+      if self.find_by(id: i) then
+        return self.find(i)
+      end
+    end
+    self.first
+  end
+
 
 
 end
