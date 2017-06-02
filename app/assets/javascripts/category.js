2a4903b25e4d95d47dfbfab0e05728e3ed1667af
github username <github email address>
2017-06-02 18:52:03 -0400

    Build next-view functionaliry for categories#show

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,9 +1,38 @@
 "use strict";
 
 $(function() {
-  $("#next-category").click(showNext(e))
+  $("#next-category").click(function(e) {
+    showNextCategory(e);
+  })
 })
 
-function showNext(e) {
-  
+function showNextCategory(e) {
+  var $id = parseInt($(e.currentTarget).attr("data-category-id")) + 1;
+  $.get(`/categories/${$id}.json`, resp => {
+    var newCategory = new Category(resp);
+    newCategory.showPage();
+  })
+}
+
+function Category(category) {
+  this.name = category.name
+  this.books = category.books
+  this.id = category.id
+}
+
+Category.prototype.showPage = function() {
+  $("#category-name").html(this.name);
+  $("#category-book-list").html("");
+  this.books.forEach(book => {
+    let listItem = this.listBook(book)
+    $("#category-book-list").append(listItem);
+  });
+  $("#next-category").attr("data-category-id", this.id);
+}
+
+Category.prototype.listBook = function(book) {
+  let html = `
+    <li><a href="/books/${book.id}">${book.title}</a> by <a href="/authors/${book.author.id}">${book.author}</a></li>
+  `
+  return html
 }
