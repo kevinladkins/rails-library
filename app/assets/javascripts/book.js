7878062cf2a44cbff3195a75ed38036ebf979b26
github username <github email address>
2017-06-03 17:59:48 -0400

    Remove coffeescript files, move global functions to own js file

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,23 +1,4 @@
-"use strict";
 
-function alphabetize(obj, attr) {
-  var alpha = obj.sort(function(a, b){
-    if(a[attr] < b[attr]) return -1;
-    if(a[attr] > b[attr]) return 1;
-    return 0;
-  })
-  return alpha
-}
-
-function orderedList() {
-  let html = `<ol></ol>`
-  return html
-}
-
-function unorderedList() {
-  let html = `<ul></ul>`
-  return html
-}
 
 
 $(function() {
