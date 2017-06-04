a97c9c69a3c9ec395dd8a3bd25c90e989980f92b
github username <github email address>
2017-06-04 13:52:58 -0400

    Refactor show-author-books with AJAX request to authors#show

--- app/assets/javascripts/author.js
+++ app/assets/javascripts/author.js
@@ -3,16 +3,29 @@
 $(function() {
   $("#show-author-books").click(function(e) {
     let authorId = $(this).attr("data-author-id");
-    $.get(`/authors/${authorId}/books.json`, books => {
+    $.get(`/authors/${authorId}.json`, author => {
+      let newAuthor = new Author(author)
       $("#author-books-list").html('')
       $("#author-books-list").html(unorderedList());
-      let alphaBooks = alphabetize(books, "title")
-      debugger
-      alphaBooks.forEach(book => {
-        let newBook = new Book(book);
-        let html = newBook.listBook();
-        $("#author-books-list ul").append(html);
+      let html = newAuthor.listBooks();
+      $("#author-books-list ul").append(html);
       });
     });
   });
-});
+
+function Author(author) {
+  this.id = author.id
+  this.name = author.name
+  this.books = alphabetize(author.books, "title")
+}
+
+Author.prototype.listBooks = function() {
+  let html = ""
+  let result = this.books.forEach(book => {
+    html += `
+      <li><a href="/books/${book.id}">${book.title}</a></li>
+    `
+  })
+  debugger
+  return html
+}
